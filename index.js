f9be0ce36c34ceba5e32de92363ff48c0ab8f126
Abrahamyan <davo.abrahamyan12@gmail.com>
2020-11-18 17:50:17 +0400

    ---

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const resolution = 5;
-const width = 2000;
-const height = 2000;
+const width = 1000;
+const height = 1000;
 const cols = width / resolution;
 const rows = height / resolution;
 let thickness = 3;
-const rand = true;
-
+const fps = 30;
 let mousePressed = false;
+const fpsElem = document.getElementById('fps');
+
 
 const rules = {
   // b: [5, 6, 7, 8],
   // s: [4, 5, 6, 7, 8],
   // ------------------
-  // b: [3],
-  // s: [2, 3],
+  b: [3],
+  s: [2, 3],
   // ------------------
   // b: [1],
   // s: [1, 2, 3, 4, 5, 6 ,7, 8]
@@ -21,71 +22,20 @@ const rules = {
   // b: [1, 2, 3, 4, 5, 6, 7, 8],
   // s: [1, 2, 3, 4, 5, 6, 7]
   // ------------------
-  b: [3, 4, 5],
-  s: [4, 5, 6, 7]
+  // b: [3, 4, 5],
+  // s: [4, 5, 6, 7]
   // ------------------
   // b: [3, 5, 6, 7],
   // s: [5, 6, 7]
 }
-
-
-let grid = matrix(cols, rows);
-let next = matrix(cols, rows)
-
-function matrix(cols, rows) {
-  const arr = new Array(rows);
-
-  for (let i = 0; i < cols; i++) {
-    arr[i] = new Array(cols)
-  }
-
-  return arr;
-}
-
-function generation() {
-  grid = next;
-  next = matrix(cols, rows);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      const state = grid[i][j];
-      const neighborsSum = neighbors(grid, i, j);
-
-      if ((state === 0 && rules.b.includes(neighborsSum)) || (state === 1 && rules.s.includes(neighborsSum))) {
-        next[i][j] = 1;
-      } else {
-        next[i][j] = 0;
-      }
-    }
-  }
-}
-
-function neighbors(grid, x, y) {
-  let sum = 0;
-
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      const col = (x + i + cols) % cols;
-      const row = (y + j + rows) % rows;
-
-      sum += grid[col][row];
-    }
-  }
-
-  sum -= grid[x][y];
-  return sum;
-}
+let grid = new Grid(cols, rows, true);
+const debug = new Debugger(grid);
+debug.registerDebugFor('draw');
+debug.registerDebugFor('generation');
 
 function setup() {
   createCanvas(width, height);
-
-  if (rand) {
-    for (let i = 0; i < cols; i++) {
-      for (let j = 0; j < rows; j++) {
-        next[i][j] = floor(random(2));
-      }
-    }
-  }
+  frameRate(fps);
 
   const canvas = document.getElementById('defaultCanvas0');
   canvas.style.border = '1px solid #000';
@@ -103,20 +53,14 @@ function setup() {
 }
 
 function draw() {
-  // background(255);
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      if (next[i][j] !== grid[i][j]) {
-        const x = i * resolution;
-        const y = j * resolution;
-        noStroke();
-        fill(next[i][j] === 1 ? 0 : 255);
-        rect(x, y, resolution, resolution);
-      }
-    }
-  }
-
-  generation();
+  debug.startDebug('draw');
+  debug.startDebug('generation');
+  const t1 = new Date().getTime();
+  grid.draw();
+  grid.generation();
+  const t2 = new Date().getTime();
+  fpsElem.innerText = 1000 / (t2 - t1);
+  console.log(debug.stopDebug('draw'), debug.stopDebug('generation'))
 }
 
 function upListener() {
@@ -136,7 +80,7 @@ function moveListener(e) {
 
     for (let I = i; I < thickness + i; I++) {
       for (let J = j; J < thickness + j; J++) {
-        next[I][J] = 1;
+        grid.grid[I][J].state = 1;
       }
     }
 
